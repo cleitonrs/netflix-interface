0bdea14e8e480f6e567a799387353c595cf03c4f
cleitonrs <rscleitonrs@gmail.com>
2021-10-11 17:15:51 -0300

    responsive updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-document.querySelector('.row-posters').addEventListener("wheel", event => {
-  if(event.deltaY > 0) {
-    event.target.scrollBy(300, 0)
-  } else {
-    event.target.scrollBy(-300, 0)
-  }
-})
+// document.querySelector('.row-posters').addEventListener("wheel", event => {
+//   if(event.deltaY > 0) {
+//     event.target.scrollBy(300, 0)
+//   } else {
+//     event.target.scrollBy(-300, 0)
+//   }
+// })
+
