12250d2c0a80c24434e596e124104818c3e94c20
cleitonrs <rscleitonrs@gmail.com>
2021-10-20 21:11:13 -0300

    images updatedes

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,8 +1,8 @@
 $('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
       0:{
           items:1
       },
