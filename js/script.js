ea67ea3da44618e008a7e45bec418f6282392651
cleitonrs <rscleitonrs@gmail.com>
2021-10-22 22:41:20 -0300

    script.js deleted

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// document.querySelector('.row-posters').addEventListener("wheel", event => {
-//   if(event.deltaY > 0) {
-//     event.target.scrollBy(300, 0)
-//   } else {
-//     event.target.scrollBy(-300, 0)
-//   }
-// })
-
